2be8cfb17efaa732e9f86cb74b699297b0550f95
Caio Barbosa Galvão <caio_bgalvao@hotmail.com>
2025-04-25 21:05:45 -0300

    fix(Vite): change root folder

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 // vite.config.js
-import 'dotenv/config'
+import "dotenv/config";
 import { defineConfig } from "vite";
 import process from "node:process";
 
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
-  root: "./",
+  root: "./src",
   server: {
     port: parseInt(process.env.VITE_PORT || process.env.PORT || "5173", 10),
     strictPort: true,
