89b1ce77181d49ff4a8ff0bdaeffc924ce5e3549
Caio Barbosa Galvão <caio_bgalvao@hotmail.com>
2025-04-27 19:59:19 -0300

    fix(Global): konami and font Opinion

--- global/global.js
+++ global/global.js
@@ -1,7 +1,5 @@
-import Konami from "konami";
-
 // Isso se faz necessário já que é assim que funciona a lib
 // eslint-disable-next-line no-unused-vars
-const easterEgg = new Konami(
+const easterEgg = new window.Konami(
   `http://caiogalvao.com.br/UVA-WebDev-AV2/contato/contato.html?promo=konamiCode`,
 );
