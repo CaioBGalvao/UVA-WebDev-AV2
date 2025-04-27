536dcaadc299f26e0a2b7814666bfef5a5108be2
Caio Barbosa Galvão <caio_bgalvao@hotmail.com>
2025-04-27 19:11:45 -0300

    fix(Global): knami easter egg

--- src/global/global.js
+++ src/global/global.js
@@ -1,19 +1,7 @@
 import Konami from "konami";
 
-const { VITE_ENV, VITE_PORT } = import.meta.env;
-
-let env = "";
-
-if (VITE_ENV !== undefined && typeof VITE_ENV === "string") {
-  if (VITE_ENV === "production") {
-    env = "http://caiogalvao.com.br/UVA-WebDev-AV2/";
-  } else {
-    env = `http://localhost:${VITE_PORT}`;
-  }
-} else {
-  throw new Error("Secret Variable not defined");
-}
-
 // Isso se faz necessário já que é assim que funciona a lib
 // eslint-disable-next-line no-unused-vars
-const easterEgg = new Konami(`${env}/contato/contato.html?promo=konamiCode`);
+const easterEgg = new Konami(
+  `http://caiogalvao.com.br/UVA-WebDev-AV2/contato/contato.html?promo=konamiCode`,
+);
